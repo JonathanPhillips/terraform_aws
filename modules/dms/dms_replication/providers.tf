b45e09906a1c86840669c0e8ced88651a5e246ab
JonathanPhillips <jon7035@gmail.com>
2024-03-26 08:46:04 -0400

    Create providers.tf

---
+++ modules/dms/dms_replication/providers.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.1.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-1"
+  profile = "sso-${lower(var.aws_env_name)}"
+  default_tags {
+    tags = {
+      "Environment"          = "${upper(var.aws_env_name)}"
+      "Pipeline_Environment" = var.env_name
+      "Pipeline_Name"        = var.pipeline_name
+      "ManagedBy"            = "Terraform"
+      "Module"               = "dms_replication"
+    }
+  }
+}
