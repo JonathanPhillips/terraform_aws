831491e24094e50ddf851f07f6d077db054fa8d6
JonathanPhillips <jon7035@gmail.com>
2024-04-10 22:46:05 -0400

    Create state.tf

---
+++ dms/dms-replication-example/state.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-states"
+    key            = "aws/us-west-2/dms/dms-replication/terraform.tfstate"
+    encrypt        = true
+    kms_key_id     = ""
+    region         = "us-west-2"
+    profile        = "sandbox"
+    dynamodb_table = "TerraformStateLock"
+  }
+}
