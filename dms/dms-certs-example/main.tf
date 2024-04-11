7f5ad14eb8ee84b9769b67657cabd20051fac23c
JonathanPhillips <jon7035@gmail.com>
2024-04-10 23:32:46 -0400

    Update main.tf

--- dms/dms-certs-example/main.tf
+++ dms/dms-certs-example/main.tf
@@ -1,5 +1,5 @@
 module "dms-certs" {
-  source = "../../../modules/dms/dms_certificate
+  source = "../../../modules/dms/dms_certificate"
   aws_env_name           = "sandbox"
   postgres_cert_filename = "rds-ca-2019-root.pem"
 }
